2f52dc5c5856aa21d370f027c308db9570b08e03
koral14 <105457134+koral14@users.noreply.github.com>
2023-10-25 21:40:37 -0700

    added a getUserByID route for User

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, register } = require('../controllers/auth');
+const { login, register, getUserById } = require('../controllers/auth');
 
+router.get('/user/:id', getUserById);
 router.post('/register', register);
 router.post('/login', login);
 
