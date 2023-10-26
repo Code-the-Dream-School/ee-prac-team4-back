0b9a8bd8b94dc5a0d93140a66885dc60c7a26e87
koral14 <105457134+koral14@users.noreply.github.com>
2023-10-25 22:08:36 -0700

    created user controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,5 +1,24 @@
 const User = require('../models/User');
 
-const register = async (req, res) => {
-    const user = await User.create({ ...req.body })
-}
+const getUserById = async (req, res) => {
+    try {
+        // extract userId from the route params
+        const userId = req.params.id;
+
+        // fetch the user by Id
+        const user = await User.findById(userId);
+
+        // check if the user exists
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        // if user exists
+        res.status(200).json(user);
+
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ message: 'Internam server error' });
+    }
+};
+
+module.exports = { getUserById };
