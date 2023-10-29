83ef368cc6329abfd27e7620c06a5a80ad6693a8
koral14 <105457134+koral14@users.noreply.github.com>
2023-10-28 19:10:36 -0700

    renamed getUserById controller into UserController

--- src/routes/User.js
+++ src/routes/User.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-const { login, register, getUserById } = require('../controllers/User');
+const { login, register, UserController } = require('../controllers/User');
 
-router.get('/user/:id', getUserById);
+router.get('/user/:id', UserController);
 router.post('/register', register);
 router.post('/login', login);
 
