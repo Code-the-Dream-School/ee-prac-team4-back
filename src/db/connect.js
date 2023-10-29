b40dbc248ec5c1dd2e05bcb2e4542f0375c2ee95
koral14 <105457134+koral14@users.noreply.github.com>
2023-10-28 19:00:19 -0700

    installed jsonwebtoken, http-status-codes, bcryptjs, and cookie

--- src/db/connect.js
+++ src/db/connect.js
@@ -3,8 +3,6 @@ const mongoose = require('mongoose');
 const connectDB = (url) => {
     return mongoose.connect(url, {
         useNewUrlParser: true,
-        useCreateIndex: true,
-        useFindAndModify: false,
         useUnifiedTopology: true,
     })
 };
--- src/server.js
+++ src/server.js
@@ -1,17 +1,17 @@
-const connectDB = require('../db/connect');
+require('dotenv').config();
 const { PORT = 8000 } = process.env;
 const app = require("./app");
+const connectDB = require('./db/connect');
+
+const listener = () => console.log(`Listening on Port ${PORT}!`);
 
 const start = async () => {
     try {
-      await connectDB(process.env.MONGO_URI)
-      app.listen(PORT,
-        console.log(`Server is listening on port ${PORT}...`)
-      )
-    } catch (error) {
-      console.log(error);
+        await connectDB(process.env.MONGO_URI)
+        app.listen(PORT, listener);
+    }  catch (error) {
+        console.log(error);
     }
-  }
-  
-  start();
-  
+};
+
+start();
