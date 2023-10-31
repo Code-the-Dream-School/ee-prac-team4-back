44773e267d1a0eca95fe323dd4ea4d6281b6685e
TWOTONE222 <jukari.richardson@gmail.com>
2023-10-30 19:38:14 -0700

    Submitting error handler functions and status code error file

--- src/app.js
+++ src/app.js
@@ -1,8 +1,13 @@
+require('dotenv').config({path:'./env'})
+require("express-async-errors");
 const express = require('express');
 const app = express();
 const cors = require('cors')
 const favicon = require('express-favicon');
 const logger = require('morgan');
+const express = require("express");
+
+const authenticateUser = require('./middleware/authentication');
 
 const mainRouter = require('./routes/mainRouter.js');
 
@@ -13,6 +18,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.static('public'))
 app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(notFoundMiddleware);
+app.use(errorHandlerMiddleware);
 
 // routes
 app.use('/api/v1', mainRouter);
