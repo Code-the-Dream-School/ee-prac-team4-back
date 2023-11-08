554341921abf02c78ba944b1dac3371f62ec1aa0
koral14 <105457134+koral14@users.noreply.github.com>
2023-11-08 15:48:30 -0800

    removed unnecessary middleware from user in app.js

--- src/app.js
+++ src/app.js
@@ -29,7 +29,6 @@ app.use(authenticateUser);app.use(cookieParser());
 
 // routes
 app.use('/api/v1', mainRouter);
-app.use('/api/v1/user', authenticateUser);
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/flashcard', authenticateUser, flashcardsRouter);
 app.use('/api/v1/flashcardsAll', allUnauthFlashcardsRouter);
