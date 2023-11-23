00b17a29b81e2e5e4f7ece2dd622cb4d806f23e1
koral14 <105457134+koral14@users.noreply.github.com>
2023-11-22 23:20:37 -0800

    updated the apis path to access the User.js

--- src/app.js
+++ src/app.js
@@ -29,12 +29,11 @@ const options = {
         },
         servers: [
             {
-                url: "http://localhost:8000",
-                url: "http://localhost:3000"
+                url: "http://localhost:8000"
             }
         ],
     },
-    apis: ["./routes/*.js"],
+    apis: ["./src/routes/*.js"],
 };
 
 const specs = swaggerJsDoc(options)
