c8c9115b808db032c60977c9315d1dae505e8586
TWOTONE222 <jukari.richardson@gmail.com>
2023-11-26 14:34:19 -0800

    Installed Resources  model and controller

---
+++ src/models/Resources.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const resourceSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Cannot be blank'],
+        maxlength: 250,
+    },
+    topic: {
+        type: String,
+        required: [true, 'Cannot be blank'],
+        maxlength: 75,
+    },
+    subtopic: {
+        type: String,
+        required: [true, 'Cannot be blank'],
+        maxlength: 150,
+    },
+    type: {
+        type: String,
+        required: [true, 'Cannot be blank'],
+        enum :['Article', 'Video', 'Game'],
+        maxlength: 75,
+
+    },
+    link: {
+        type: String,
+        required: [true, 'Cannot be blank']
+    }
+}, );
+
+module.exports = mongoose.model('Resources', resourceSchema);
