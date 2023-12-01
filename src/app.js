dd34b53e18a79a19f48cf937457e81c41c97e2be
koral14 <105457134+koral14@users.noreply.github.com>
2023-11-30 17:50:54 -0800

    removed unique property for username only for testing purposes

--- src/app.js
+++ src/app.js
@@ -20,7 +20,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(logger('dev'));
-app.use(express.static('public'))
+app.use(express.static('public'));
 app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(cookieParser());
 
--- src/models/User.js
+++ src/models/User.js
@@ -5,19 +5,19 @@ const userSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Cannot be blank'],
         match: [/^[a-zA-Z0-9]+$/, 'Username is invalid'],
-        unique: [true, 'This username is already taken'],
+        // unique: [true, 'This username is already taken'], // for testing purposes
         maxlength: 50,
     },
     firstName: {
         type: String,
-        required: [false, 'Cannot be blank'],
+        required: [false, 'Cannot be blank'], // false only for testing purposes
         match: [/^[a-zA-Z0-9]+$/, 'Username is invalid'],
         maxlength: 50,
     },
     lastName: {
         type: String,
-        required: [false, 'Cannot be blank'],
-        match: [/^[a-zA-Z0-9]+$/, 'Username is invalid'],
+        required: [false, 'Cannot be blank'],  // false only for testing purposes
+        match: [/^[a-zA-Z0-9]+$/, 'Username is invalid'], 
         maxlength: 50,
     },
     role: {
