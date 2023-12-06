5e0ad5a7b139a2cb89c93ea78a4a76cddc4ae9fe
dylan <ddigioia3@gmail.com>
2023-12-05 20:34:26 -0500

    PRACT-81 Fix Expires Time on JWT

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 function createJWT(user, expiresIn) {
-    expiresIn = expiresIn || process.env.JWT_LIFETIME;
+    expiresIn = +(expiresIn || process.env.JWT_LIFETIME);
     return jwt.sign(
         { userId: user._id, username: user.username },
         process.env.JWT_SECRET,
