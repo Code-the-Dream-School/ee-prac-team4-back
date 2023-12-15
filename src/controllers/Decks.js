b6d5eb6096a614e03767f5854cd5470e26390f8a
Miguel <74852114+madebymiguel@users.noreply.github.com>
2023-12-15 13:22:52 -0800

    Remove the limit and page offsets just so we can get all items. Can be added back later.

--- src/controllers/Decks.js
+++ src/controllers/Decks.js
@@ -1,224 +1,233 @@
-const Flashcard = require('../models/Flashcard');
-const User = require('../controllers/User');
-const Deck = require('../models/Deck');
-const { StatusCodes } = require('http-status-codes');
-const { getDetailedFlashcards, getUserById } = require('../utils/utilFuctions');
+const Flashcard = require("../models/Flashcard");
+const User = require("../controllers/User");
+const Deck = require("../models/Deck");
+const { StatusCodes } = require("http-status-codes");
+const { getDetailedFlashcards, getUserById } = require("../utils/utilFuctions");
 
 // get all decks by unauthenticated users
 const getAllDecks = async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+  // Can add these back
+  // const page = parseInt(req.query.page) || 1;
+  // const limit = parseInt(req.query.limit) || 10;
 
-    try {
-        const offset = (page -1) * limit;
-        const totalDecks = await Deck.countDocuments({ isPublic: true });
-        const decks = await Deck.find({ isPublic: true })
-            .sort('createdAt')
-            .skip(offset)
-            .limit(limit);
+  try {
+    // const offset = (page - 1) * limit;
+    const totalDecks = await Deck.countDocuments({ isPublic: true });
+    const decks = await Deck.find({ isPublic: true }).sort("createdAt");
 
-            const decksWithDetailedFlashcards = await Promise.all(decks.map(async (deck) => {
-                const detailedFlashcards = await getDetailedFlashcards(deck.flashcards);
-                return {
-                    ...deck.toObject(),
-                    flashcards: detailedFlashcards,
-                };
-            }));
-    
-        res.status(StatusCodes.OK).json({
-            decks: decksWithDetailedFlashcards,
-            currentPage: page,
-            totalPages: Math.ceil(totalDecks / limit),
-            totalItems: totalDecks,
-        });
+    const decksWithDetailedFlashcards = await Promise.all(
+      decks.map(async (deck) => {
+        const detailedFlashcards = await getDetailedFlashcards(deck.flashcards);
+        return {
+          ...deck.toObject(),
+          flashcards: detailedFlashcards,
+        };
+      }),
+    );
 
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error'});
-    }
+    res.status(StatusCodes.OK).json({
+      decks: decksWithDetailedFlashcards,
+      //   currentPage: page,
+      //   totalPages: Math.ceil(totalDecks / limit),
+      totalItems: totalDecks,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // get all user decks
 const getUserDecks = async (req, res) => {
-
-    try {
-        if (!req.user || !req.user.userId) {
-            return res.status(StatusCodes.BAD_REQUEST).json({ message: 'Invalid user ID' });
-        }
-        
-        // added pagination
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
-
-        const user = await getUserById(req.user.userId);
-
-        const offset = (page - 1) * limit;
-        const totalDecks = await Deck.countDocuments({ createdBy: req.user.userId });
-        const decks = await Deck.find({ createdBy: req.user.userId })
-            .sort('createdAt')
-            .skip(offset)
-            .limit(limit);
-
-            // Fetch detailed flashcards for each deck
-            const decksWithDetailedFlashcards = await Promise.all(decks.map(async (deck) => {
-
-                if (deck.flashcards.length === 0) {
-                    return {
-                        ...deck.toObject(),
-                        flashcards: [], 
-                    };
-                }
-                
-                const detailedFlashcards = await getDetailedFlashcards(deck.flashcards);
-    
-                // Decorate each deck with detailed flashcards
-                return {
-                    ...deck.toObject(),
-                    flashcards: detailedFlashcards,
-                };
-            }));
-
-            const usersFavorites = user.favorite_decks;
-            for (let deck of decksWithDetailedFlashcards) {
-                deck.isFavorite = usersFavorites.includes(deck._id);
-            }
-
-        res.status(StatusCodes.OK).json({
-            decks: decksWithDetailedFlashcards,
-            currentPage: page,
-            totalPages: Math.ceil(totalDecks / limit),
-            totalItems: totalDecks,
-        });
-
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error'});
+  try {
+    if (!req.user || !req.user.userId) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ message: "Invalid user ID" });
     }
-};
 
-// get detailed deck information with detailed flashcards 
-const getDeckWithFlashcards = async (req, res) => {
-    try {
-        const deckId = req.params.id; 
-        const page = parseInt(req.query.page) || 1;
-        const limit = parseInt(req.query.limit) || 10;
-        const skip = (page - 1) * limit;
+    // added pagination
+    // const page = parseInt(req.query.page) || 1;
+    // const limit = parseInt(req.query.limit) || 10;
 
-        const deck = await Deck.findById(deckId)
-            .populate({
-                path: 'flashcards',
-                options: { skip, limit },
-            });
+    const user = await getUserById(req.user.userId);
 
-        if (!deck) {
-            return res.status(StatusCodes.NOT_FOUND).json({ msg: `No deck with id ${deckId}` });
+    // const offset = (page - 1) * limit;
+    const totalDecks = await Deck.countDocuments({
+      createdBy: req.user.userId,
+    });
+    const decks = await Deck.find({ createdBy: req.user.userId }).sort(
+      "createdAt",
+    );
+
+    // Fetch detailed flashcards for each deck
+    const decksWithDetailedFlashcards = await Promise.all(
+      decks.map(async (deck) => {
+        if (deck.flashcards.length === 0) {
+          return {
+            ...deck.toObject(),
+            flashcards: [],
+          };
         }
 
         const detailedFlashcards = await getDetailedFlashcards(deck.flashcards);
 
-        res.status(StatusCodes.OK).json({
-            deck: {
-                _id: deck._id,
-                topic: deck.topic,
-                subtopic: deck.subtopic,
-                title: deck.title,
-                isPublic: deck.isPublic,
-                createdBy: deck.createdBy,
-                flashcards: detailedFlashcards,
-                createdAt: deck.createdAt,
-                updatedAt: deck.updatedAt,
-                __v: deck.__v,
-            }
-        });
+        // Decorate each deck with detailed flashcards
+        return {
+          ...deck.toObject(),
+          flashcards: detailedFlashcards,
+        };
+      }),
+    );
 
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error' })
+    const usersFavorites = user.favorite_decks;
+    for (let deck of decksWithDetailedFlashcards) {
+      deck.isFavorite = usersFavorites.includes(deck._id);
     }
+
+    res.status(StatusCodes.OK).json({
+      decks: decksWithDetailedFlashcards,
+      //   currentPage: page,
+      //   totalPages: Math.ceil(totalDecks / limit),
+      totalItems: totalDecks,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
+};
+
+// get detailed deck information with detailed flashcards
+const getDeckWithFlashcards = async (req, res) => {
+  try {
+    const deckId = req.params.id;
+    // const page = parseInt(req.query.page) || 1;
+    // const limit = parseInt(req.query.limit) || 10;
+    // const skip = (page - 1) * limit;
+
+    const deck = await Deck.findById(deckId).populate({
+      path: "flashcards",
+      //   options: { skip, limit },
+    });
+
+    if (!deck) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: `No deck with id ${deckId}` });
+    }
+
+    const detailedFlashcards = await getDetailedFlashcards(deck.flashcards);
+
+    res.status(StatusCodes.OK).json({
+      deck: {
+        _id: deck._id,
+        topic: deck.topic,
+        subtopic: deck.subtopic,
+        title: deck.title,
+        isPublic: deck.isPublic,
+        createdBy: deck.createdBy,
+        flashcards: detailedFlashcards,
+        createdAt: deck.createdAt,
+        updatedAt: deck.updatedAt,
+        __v: deck.__v,
+      },
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // create deck according to the schema
 const createDeck = async (req, res) => {
-    req.body.createdBy = req.user.userId;
-    const deck = await Deck.create(req.body);
-    res.status(StatusCodes.CREATED).json({ deck });
+  req.body.createdBy = req.user.userId;
+  const deck = await Deck.create(req.body);
+  res.status(StatusCodes.CREATED).json({ deck });
 };
 
 // update deck
 const updateDeck = async (req, res) => {
-    const {
-        body: { topic, subtopic, title, isPublic, createdBy, flashcards },
-        user: { userId },
-        params: { id: deckId }
-    } = req;
+  const {
+    body: { topic, subtopic, title, isPublic, createdBy, flashcards },
+    user: { userId },
+    params: { id: deckId },
+  } = req;
 
-    const updatedFields = {};
+  const updatedFields = {};
 
-    if (topic !== undefined) {
-        updatedFields.topic = topic;
+  if (topic !== undefined) {
+    updatedFields.topic = topic;
+  }
+
+  if (subtopic !== undefined) {
+    updatedFields.subtopic = subtopic;
+  }
+
+  if (title !== undefined) {
+    updatedFields.title = title;
+  }
+
+  if (isPublic !== undefined) {
+    updatedFields.isPublic = isPublic;
+  }
+
+  if (flashcards !== undefined) {
+    updatedFields.flashcards = flashcards;
+  }
+
+  if (Object.keys(updatedFields).length === 0) {
+    return res.status(400).json({ msg: "No fields to update" });
+  }
+
+  try {
+    const deck = await Deck.findOneAndUpdate(
+      { _id: deckId, createdBy: userId },
+      { $set: updatedFields },
+      { new: true, runValidators: true },
+    );
+
+    if (!deck) {
+      return res.status(404).json({ msg: `No deck with id ${deckId}` });
     }
 
-    if (subtopic !== undefined) {
-        updatedFields.subtopic = subtopic;
-    }
-
-    if (title !== undefined) {
-        updatedFields.title = title;
-    }
-
-    if (isPublic !== undefined) {
-        updatedFields.isPublic = isPublic;
-    }
-
-    if (flashcards !== undefined) {
-        updatedFields.flashcards = flashcards;
-    }
-
-    if (Object.keys(updatedFields).length === 0) {
-        return res.status(400).json({ msg: 'No fields to update' })
-    }
-
-    try {
-        const deck = await Deck.findOneAndUpdate(
-            { _id: deckId, createdBy: userId },
-            { $set: updatedFields },
-            { new: true, runValidators: true }
-        );
-
-        if (!deck) {
-            return res.status(404).json({ msg: `No deck with id ${deckId}` });
-        }
-
-        res.status(StatusCodes.OK).json({ deck });
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error' });
-    }
+    res.status(StatusCodes.OK).json({ deck });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // delete deck
 const deleteDeck = async (req, res) => {
-    const {
-        user: { userId },
-        params: { id: deckId }
-    } = req;
+  const {
+    user: { userId },
+    params: { id: deckId },
+  } = req;
 
-    const deck = await Deck.findOneAndDelete(
-        { _id: deckId, createdBy: userId }
-    );
+  const deck = await Deck.findOneAndDelete({ _id: deckId, createdBy: userId });
 
-    if (!deck) {
-        return res.status(StatusCodes.NOT_FOUND).json({ msg: `No deck with id ${deckId}` });
-    };
+  if (!deck) {
+    return res
+      .status(StatusCodes.NOT_FOUND)
+      .json({ msg: `No deck with id ${deckId}` });
+  }
 
-    res.status(StatusCodes.OK).json({ msg: 'Deck deleted' });
+  res.status(StatusCodes.OK).json({ msg: "Deck deleted" });
 };
 
 module.exports = {
-    getAllDecks,
-    getUserDecks,
-    getDeckWithFlashcards,
-    createDeck,
-    updateDeck,
-    deleteDeck
-}
+  getAllDecks,
+  getUserDecks,
+  getDeckWithFlashcards,
+  createDeck,
+  updateDeck,
+  deleteDeck,
+};
--- src/controllers/Flashcards.js
+++ src/controllers/Flashcards.js
@@ -1,167 +1,179 @@
-const Flashcard = require('../models/Flashcard');
-const Deck = require('../models/Deck');
-const { StatusCodes } = require('http-status-codes');
+const Flashcard = require("../models/Flashcard");
+const Deck = require("../models/Deck");
+const { StatusCodes } = require("http-status-codes");
 
 // GET all flashcards for any unauthenticated user
 const getAllFlashcards = async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+  // const page = parseInt(req.query.page) || 1;
+  // const limit = parseInt(req.query.limit) || 10;
 
-    try {
-        const offset = (page - 1) * limit;
-        const totalFlashcards = await Flashcard.countDocuments({});
-        const flashcards = await Flashcard.find({})
-            .sort('createdAt')
-            .skip(offset)
-            .limit(limit);
+  try {
+    // const offset = (page - 1) * limit;
+    const totalFlashcards = await Flashcard.countDocuments({});
+    const flashcards = await Flashcard.find({}).sort("createdAt");
 
-            res.status(StatusCodes.OK).json({ 
-                flashcards, 
-                currentPage: page,
-                totalPages: Math.ceil(totalFlashcards / limit),
-                totalItems: totalFlashcards, 
-            });
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error' })
-    }
+    res.status(StatusCodes.OK).json({
+      flashcards,
+      //   currentPage: page,
+      //   totalPages: Math.ceil(totalFlashcards / limit),
+      totalItems: totalFlashcards,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // GET the flashcards of a specific user (for a logged in user)
 const getUserFlashcards = async (req, res) => {
-    const page = parseInt(req.query.page) || 1;
-    const limit = parseInt(req.query.limit) || 10;
+  //   const page = parseInt(req.query.page) || 1;
+  //   const limit = parseInt(req.query.limit) || 10;
 
-    try {
-        const offset = (page - 1) * limit;
-        const totalFlashcards = await Flashcard.countDocuments({ createdBy: req.user.userId });
-        const flashcards = await Flashcard.find({ createdBy: req.user.userId })
-            .sort('createdAt')
-            .skip(offset)
-            .limit(limit);
+  try {
+    // const offset = (page - 1) * limit;
+    const totalFlashcards = await Flashcard.countDocuments({
+      createdBy: req.user.userId,
+    });
+    const flashcards = await Flashcard.find({
+      createdBy: req.user.userId,
+    }).sort("createdAt");
 
-            res.status(StatusCodes.OK).json({ 
-                flashcards, 
-                currentPage: page,
-                totalPages: Math.ceil(totalFlashcards / limit),
-                totalItems: totalFlashcards, 
-            });
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error' })
-    }
+    res.status(StatusCodes.OK).json({
+      flashcards,
+      //   currentPage: page,
+      //   totalPages: Math.ceil(totalFlashcards / limit),
+      totalItems: totalFlashcards,
+    });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // get one flashcard
 const getFlashcard = async (req, res) => {
-    const { 
-        user: { userId }, 
-        params: { id: flashcardId } 
-    } = req;
+  const {
+    user: { userId },
+    params: { id: flashcardId },
+  } = req;
 
-    try {
-        const flashcard = await Flashcard.findOne({
-            _id: flashcardId, createdBy: userId
-        });
-    
-        if (!flashcard) {
-            return res.status(StatusCodes.NOT_FOUND).json({ msg: `No flashcard with id ${flashcardId}` });
-        }
-    
-        res.status(StatusCodes.OK).json({ flashcard });
-        
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: "Internal server error" });
+  try {
+    const flashcard = await Flashcard.findOne({
+      _id: flashcardId,
+      createdBy: userId,
+    });
+
+    if (!flashcard) {
+      return res
+        .status(StatusCodes.NOT_FOUND)
+        .json({ msg: `No flashcard with id ${flashcardId}` });
     }
-    
+
+    res.status(StatusCodes.OK).json({ flashcard });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // create flashcard according to the schema
 const createFlashcard = async (req, res) => {
-    try {
-        req.body.createdBy = req.user.userId;
+  try {
+    req.body.createdBy = req.user.userId;
 
-        const flashcard = await Flashcard.create(req.body);
-        const deckId = req.body.deck; 
+    const flashcard = await Flashcard.create(req.body);
+    const deckId = req.body.deck;
 
-        const updatedDeck = await Deck.findByIdAndUpdate(deckId, {
-            $push: { flashcards: flashcard._id },
-        }, { new: true }); 
-        
-        res.status(StatusCodes.CREATED).json({ flashcard });
-    } catch (error) {
-        console.error(error);
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).json({ msg: 'Internal server error' });
-    }    
+    const updatedDeck = await Deck.findByIdAndUpdate(
+      deckId,
+      {
+        $push: { flashcards: flashcard._id },
+      },
+      { new: true },
+    );
+
+    res.status(StatusCodes.CREATED).json({ flashcard });
+  } catch (error) {
+    console.error(error);
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ msg: "Internal server error" });
+  }
 };
 
 // update flashcard
 const updateFlashcard = async (req, res) => {
-    const {
-        body: { question, answer, resources, hint, createdBy },
-        user: { userId },
-        params: { id: flashcardId }
-    } = req;
+  const {
+    body: { question, answer, resources, hint, createdBy },
+    user: { userId },
+    params: { id: flashcardId },
+  } = req;
 
-    const updatedFields = {};
+  const updatedFields = {};
 
-    if (question !== undefined) {
-        updatedFields.question = question;
-    }
+  if (question !== undefined) {
+    updatedFields.question = question;
+  }
 
-    if (answer !== undefined) {
-        updatedFields.answer = answer;
-    }
+  if (answer !== undefined) {
+    updatedFields.answer = answer;
+  }
 
-    if (resources !== undefined) {
-        updatedFields.resources = resources;
-    }
+  if (resources !== undefined) {
+    updatedFields.resources = resources;
+  }
 
-    if (hint !== undefined) {
-        updatedFields.hint = hint;
-    }
+  if (hint !== undefined) {
+    updatedFields.hint = hint;
+  }
 
-    if (Object.keys(updatedFields).length === 0) {
-        return res.status(400).json({ msg: 'No fields to update' });
-    }
+  if (Object.keys(updatedFields).length === 0) {
+    return res.status(400).json({ msg: "No fields to update" });
+  }
 
-    const flashcard = await Flashcard.findByIdAndUpdate(
-        { _id: flashcardId, createdBy: userId },
-        { $set: updatedFields },
-        { new: true, runValidators: true }
-    )
+  const flashcard = await Flashcard.findByIdAndUpdate(
+    { _id: flashcardId, createdBy: userId },
+    { $set: updatedFields },
+    { new: true, runValidators: true },
+  );
 
-    if (!flashcard) {
-        return res.status(400).json({ msg: `No flashcard with id ${flashcardId}` });
-    }
+  if (!flashcard) {
+    return res.status(400).json({ msg: `No flashcard with id ${flashcardId}` });
+  }
 
-    res.status(StatusCodes.OK).json({ flashcard });
-}
+  res.status(StatusCodes.OK).json({ flashcard });
+};
 
 // delete flashcard
 const deleteFlashcard = async (req, res) => {
-    const {
-        user: { userId },
-        params: { id: flashcardId }
-    } = req;
+  const {
+    user: { userId },
+    params: { id: flashcardId },
+  } = req;
 
-    const flashcard = await Flashcard.findOneAndDelete(
-        { _id: flashcardId, createdBy: userId }
-    );
+  const flashcard = await Flashcard.findOneAndDelete({
+    _id: flashcardId,
+    createdBy: userId,
+  });
 
-    if(!flashcard) {
-        return res.status(400).json({ msg: `No flashcard with id ${flashcardId}` });
-    };
+  if (!flashcard) {
+    return res.status(400).json({ msg: `No flashcard with id ${flashcardId}` });
+  }
 
-    res.status(StatusCodes.OK).json({ msg: "Flashcard deleted" });
+  res.status(StatusCodes.OK).json({ msg: "Flashcard deleted" });
 };
 
 module.exports = {
-    getAllFlashcards,
-    getUserFlashcards,
-    getFlashcard,
-    createFlashcard,
-    updateFlashcard,
-    deleteFlashcard,
-}
+  getAllFlashcards,
+  getUserFlashcards,
+  getFlashcard,
+  createFlashcard,
+  updateFlashcard,
+  deleteFlashcard,
+};
